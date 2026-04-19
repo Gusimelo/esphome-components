485a99408053033f116ce435dd42a1559bbc4cd7
Gusimelo <53083182+Gusimelo@users.noreply.github.com>
2026-04-19 21:02:43 +0100

    Remove unused WiFi and WiFiUDP dependencies
    Removed unused WiFi and WiFiUDP includes and client instances.

--- components/wmbus/wmbus.h
+++ components/wmbus/wmbus.h
@@ -14,9 +14,6 @@
 
 #include "drivers.h"
 
-#include <WiFi.h>
-#include <WiFiUdp.h>
-
 #include "rf_cc1101.h"
 #include "m_bus_data.h"
 #include "utils.h"
@@ -131,8 +128,6 @@ namespace wmbus {
       std::map<uint32_t, WMBusListener *> wmbus_listeners_{};
       std::map<std::string, Driver *> drivers_{};
       std::vector<Client> clients_{};
-      WiFiClient tcp_client_;
-      WiFiUDP udp_client_;
       time::RealTimeClock *time_{nullptr};
       uint32_t led_blink_time_{0};
       uint32_t led_on_millis_{0};
